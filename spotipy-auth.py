7b6717c85c745a4b9eb1bedeac97bc118e1e22e6
bseibo61 <brseibol@buffalo.edu>
2018-01-15 23:39:09 -0500

    merging spotipy and flask

--- spotipy-auth.py
+++ spotipy-auth.py
@@ -1,15 +1,18 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
 
 def auth():
     # Want to export these later
     client_id = "a843addfdfca4298ae0d7abb074e5c94"
     client_secret = "7062c3bbd0a34b3697bf4bff1680f272"
+    username = "crimefightingcoconut"
+    scope = "user-read-currently-playing"
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     return client_credentials_manager
 
-util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=localhost:8080/callback)
-
+token = util.prompt_for_user_token("crimefightingcoconut","user-read-currently-playing",client_id="a843addfdfca4298ae0d7abb074e5c94",client_secret="7062c3bbd0a34b3697bf4bff1680f272",redirect_uri="http://127.0.0.1:8080/callback/")
+client_id
 
 sp = spotipy.Spotify(client_credentials_manager=auth())
 sp.trace = True # turn on tracing
